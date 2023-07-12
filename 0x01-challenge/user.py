fadd8bf5ad054ea688febab72487ab4e75918f60
OBI CHIEMERIE     ("THALES") <117930136+WINNER2028@users.noreply.github.com>
2023-07-12 21:30:10 +0100

    Update user.py

--- 0x01-challenge/user.py
+++ 0x01-challenge/user.py
@@ -1,31 +1,24 @@
-#!/usr/bin/python3
-"""
-User class
-"""
-
-
-class User():
-    """ Documentation """
+class User:
+    """Documentation"""
 
     def __init__(self):
-        """ Documentation """
-        self.__email = email
-
-    @email.setter
-    def email(self, value):
-        """ Documentation """
-        if type(value) is not str:
-            raise TypeError("email must be a string")
-        self.__email = value
+        """Documentation"""
+        self.__email = ""
 
     @property
     def email(self):
-        """ Documentation """
+        """Documentation"""
         return self.__email
+
+    @email.setter
+    def email(self, value):
+        """Documentation"""
+        if not isinstance(value, str):
+            raise TypeError("email must be a string")
+        self.__email = value
 
 
 if __name__ == "__main__":
-
     u = User()
     u.email = "john@stone.com"
     print(u.email)
